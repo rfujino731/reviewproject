20082ba47ba590ea54ed153ed789fdb9456efb75
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-10-21 23:43:02 +0900

    modified reviewpost/views.py

--- reviewpost/views.py
+++ reviewpost/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
 
 def signupview(request):
     if request.method == 'POSt':
-        username_date = request.POST['username_date']
-        password_date = request.POST['password_date']
-        user = User.objects.create_user(username_date, '', password_date)
-        print('OK')
+       print('POST method')
     else:
-        print("else")
-        print(User.objects.all())
-        return render(request, 'reviewpost/signup.html', {})
+        print("GET method probably")
     return render(request, 'reviewpost/signup.html', {})
