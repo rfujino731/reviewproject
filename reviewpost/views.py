b995f9cbc85f079ba448e9247d630235ed96a1a5
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-10-21 23:35:14 +0900

    add signup.html

--- reviewpost/views.py
+++ reviewpost/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+def signupview(request):
+    if request.method == 'POSt':
+        username_date = request.POST['username_date']
+        password_date = request.POST['password_date']
+        user = User.objects.create_user(username_date, '', password_date)
+        print('OK')
+    else:
+        print("else")
+        print(User.objects.all())
+        return render(request, 'reviewpost/signup.html', {})
+    return render(request, 'reviewpost/signup.html', {})
