3bf80f38975ca433f39206980a9efa1192654d38
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-11-04 00:03:10 +0900

    modified views.py add try except prohibition of registration of duplicate dataS

--- reviewpost/views.py
+++ reviewpost/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 def signupview(request):
     if request.method == 'POST':
-        print("OK")
-       # username_date = request.POST['username_data']
-       # password_date = request.POST['password_data']
-       # user = User.object.create_user(username_date, '', password_date)
+        username_date = request.POST['username_data']
+        password_date = request.POST['password_data']
+        try:
+            user = User.objects.create_user(username_date, '', password_date)
+        except IntegrityError:
+            return render(request, 'reviewpost/signup.html', {'error': '既に登録されています。'})
     else:
-        render(request, 'reviewpost/signup.html', {})
+        #print(User.objects.all())
+        return render(request, 'reviewpost/signup.html', {})
     return render(request, 'reviewpost/signup.html', {})
