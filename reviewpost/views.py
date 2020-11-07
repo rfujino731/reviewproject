cfb0e5214110100af2a86adcbf5563439cff8bd4
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-11-07 23:42:01 +0900

    add def_loginview to views.py

--- reviewpost/views.py
+++ reviewpost/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.contrib.auth import authenticate, login
 
 def signupview(request):
     if request.method == 'POST':
@@ -14,3 +15,15 @@ def signupview(request):
         #print(User.objects.all())
         return render(request, 'reviewpost/signup.html', {})
     return render(request, 'reviewpost/signup.html', {})
+
+def loginview(request):
+    if request.mrthod == 'POST':
+        username_date = request.POST['username_data']
+        password_data = request.POST['password_data']
+        user = authenticate(request, username=username_date, password=password_data)
+        if user is not None:
+            login(request, user)
+            return redirect('list')
+        else:
+            return redirect('login')
+    return render(request, 'login.html')
